007557fe44ed4aed664e99bb95cbf5a89937f328
Marco F <marco.firrincieli@unicredit.eu>
2019-09-30 14:25:50 +0200

    readConfigFromFile added. This closes #1

--- src/main/scala/com/wanari/renervator/Config.scala
+++ src/main/scala/com/wanari/renervator/Config.scala
@@ -9,4 +9,14 @@ object Config {
     List(Host("test", "127.0.0.1", "AA:AA:AA:AA:AA:AA"))
   }
 
+  // This assumes files are three-columned comma-separated
+  def readConfigFromFile(path: String) = IO{
+    scala.io.Source.fromFile(path)
+    .getLines()
+    .map(_.split(","))
+    .map { case a =>
+      Host(a(0), a(1), a(2))
+    }.toList
+  }
+
 }
